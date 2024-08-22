064bcd4256b85ace047c23195fe2ec9b66fd710d
Vimala242 <vimalam242@gmail.com>
2024-08-21 22:05:39 -0400

    Adding changes for Proxy services of Backend APIs

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,14 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
-import Registration from './components/Register/Register'; // Import the Registration component
+import Registration from './components/Register/Register'; 
+import Dealers from './components/Dealers/Dealers';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Registration />} /> {/* Add route for registration */}
+      <Route path="/register" element={<Registration />} /> 
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
