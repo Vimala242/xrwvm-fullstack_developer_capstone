e1ec32c5a7a99047b9e98d9bf6ec7d6713072281
Vimala242 <vimalam242@gmail.com>
2024-08-21 21:09:29 -0400

    Adding CarMake and CarModel changes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,19 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'founded_year', 'headquarters_location')
+    search_fields = ('name', 'description')
+    list_filter = ('founded_year', 'headquarters_location')
+    ordering = ('name',)  
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'make', 'type', 'year', 'dealer_id', 'color', 'engine_size')
+    list_filter = ('make', 'type', 'year')
+    search_fields = ('name', 'make__name', 'dealer_id')
+    ordering = ('-year',)  
 
-# CarModelInline class
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,38 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)  
+    founded_year = models.PositiveIntegerField(blank=True, null=True)  
+    headquarters_location = models.CharField(max_length=255, blank=True, null=True)  
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 
+class CarModel(models.Model):
+    CAR_TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('COUPE', 'Coupe')
+    ]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='car_models')
+    dealer_id = models.IntegerField()   
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50, choices=CAR_TYPE_CHOICES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    color = models.CharField(max_length=50, blank=True, null=True)  
+    engine_size = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)  
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year.year})"
